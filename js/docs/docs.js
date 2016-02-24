5b5659f0cd31daa5f7aa308f163aa28012e4fb27
Lee Jordan <ldjordan@gmail.com>
2016-02-24 17:50:26 +0000

    add new header including pistachio image

--- js/docs/docs.js
+++ js/docs/docs.js
@@ -1,9 +1,10 @@
 (function(document) {
+    var $h1 = document.querySelectorAll('h1.cd-title');
     var $h2 = document.querySelectorAll('h2.cd-title');
     var $pageNav = document.querySelector('.cd-page-nav');
     var i;
 
-    if ($pageNav && $h2.length > 1) {
+    if ($pageNav && $h1.length > 0 && $h2.length > 1) {
         $pageNav.className += ' cd-page-nav--show';
 
         for (var i = 0; i < $h2.length; i ++) {
