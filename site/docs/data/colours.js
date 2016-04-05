0c51a7dbd6f37e694ad9a42359131bc5d62dadba
Lee Jordan <ldjordan@gmail.com>
2016-04-05 11:40:26 +0100

    adds secondary colours and builds colour page from json

---
+++ site/docs/data/colours.js
@@ -0,0 +1,85 @@
+
+
+// List of colours
+module.exports = {
+    brand: {
+        primary: [
+            {
+                title: 'electric moss',
+                variable: '@colour-brand',
+                hex: '00989e'
+            }
+        ],
+        secondary: [
+            {
+                title: 'lemon',
+                variable: '@colour-brand-lemon',
+                hex: 'fcc444'
+            },
+            {
+                title: 'lime',
+                variable: '@colour-brand-lime',
+                hex: 'b0c148'
+            },
+            {
+                title: 'apple',
+                variable: '@colour-brand-apple',
+                hex: 'e26859'
+            },
+            {
+                title: 'berry',
+                variable: '@colour-brand-berry',
+                hex: '873e70'
+            }
+        ]
+    },
+    ui: [
+        {
+            title: 'accent',
+            variable: '@colour-accent',
+            hex: '5d9f31'
+        },
+        {
+            title: 'error',
+            variable: '@colour-error',
+            hex: 'ab1818'
+        }
+    ],
+    monochrome: [
+        {
+            title: 'white',
+            variable: '@colour-white',
+            hex: 'ffffff'
+        },
+        {
+            title: 'black',
+            variable: '@colour-black',
+            hex: '000000'
+        },
+        {
+            title: 'grey-dark',
+            variable: '@colour-grey-dark',
+            hex: '333333'
+        },
+        {
+            title: 'grey',
+            variable: '@colour-grey',
+            hex: '555555'
+        },
+        {
+            title: 'grey-light',
+            variable: '@colour-grey-light',
+            hex: '888888'
+        },
+        {
+            title: 'grey-lighter',
+            variable: '@colour-grey-lighter',
+            hex: 'cccccc'
+        },
+        {
+            title: 'grey-lightest',
+            variable: '@colour-grey-lightest',
+            hex: 'eeeeee'
+        }
+    ]
+}
